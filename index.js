7589bf3de7ff6ce48797420129b5afde412d0f64
Dharmesh-Sookun <sookundharmesh2906@gmail.com>
2022-12-28 16:26:48 +0400

    Get port from env and remove cors restriction

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
-
-const corsOption = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200,
-};
+const port = process.env.PORT || 5000;
 
 const transport = {
   host: 'smtp.gmail.com',
@@ -37,7 +32,7 @@ const limiter = rateLimit({
 });
 
 app.use(helmet());
-app.use(cors(corsOption));
+app.use(cors());
 app.use(express.json());
 app.use(morgan('common'));
 app.use(limiter);
